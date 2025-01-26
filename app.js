dca5de210d94821ea65816d278c2d5fa7c6155bc
Tortilla Dura <pinedaperezjenari@gmail.com>
2025-01-25 21:32:13 -0600

    actualiza

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    
+    if (nombre == "") {
+        alert("Por favor, agrega un nombre valido")
+    } else {
+        amigos.push(nombre);
+    }
+    
+    console.log(amigos)
+    
+    document.getElementById("amigo").value = "";
+
+    lista();
+
+    return;
+}
+
+
+// creamos la funcion
+function lista() {
+    const listaAmigos = document.getElementById('listaAmigos');    //Seleccionamos la cajita html
+    listaAmigos.innerHTML = "";                                    //Borramos los datos que se encuentran en la lista para que no se dubpliquen    
+    amigos.forEach(amigos => {                                      
+        const li = document.createElement('li');                   //por cada dato creamos elemento <li>
+        li.textContent = amigos;                                   //Asigna el nombre del amigo al contenido del <li>
+        listaAmigos.appendChild(li);                               //Agrega el elemento <li> a la lista <ul>
+      });
+    return;
+}
+
+
+
+function sortearAmigo() {
+    if (amigos == "") {
+        alert("Aun no has escrito ningun nombre :c");
+
+    } else {
+        const cantidadAmigos = amigos.length;
+        const indice = Math.floor(Math.random()*cantidadAmigos)+1;
+        console.log(indice);
+        listaAmigos.innerHTML = "";
+
+
+        const resultado = document.getElementById('resultado');
+        resultado.innerHTML = `El amigo secreto sorteado es: ${amigos[indice-1]}`;
+    }
+    return;
+}
